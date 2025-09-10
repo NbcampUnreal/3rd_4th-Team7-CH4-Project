5b4b1aeb288a899b38e2de8392aee96aef55c8e3
Juyee Myeong <juyee.ming@gmail.com>
2025-09-10 13:53:21 +0900

    [chore] Gameplay Tag 정림 및 게임 플로우 Gameplay 태그로 관리
    - DefaultGameplayTags.ini 정리:
      - 추가: State.Movement.Sprinting, Status.Stamina.Empty, Effect.Stamina.Drain  및 Game Phase 관리와 캐릭터 선택 시 활용된 태그
      - 유지/정비: Ability.Sprint, Effect.Stamina.Regen, Game.Phase.*, Player.*
    - 네이티브 태그 선언/정의:
      - GameFlowTags.h / GameFlowTags.cpp에 모든 태그 UE_DECLARE/UE_DEFINE 추가

---
+++ Source/TreasureHunter/Game/GameFlowTags.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "NativeGameplayTags.h"
+
+// ----- Ability -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Sprint);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Mantle);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Push);
+
+
+// ----- Effect -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Effect_Stamina_Regen);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Effect_Stamina_Drain);
+
+// ----- Movement State -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_State_Movement_Sprinting);
+
+// ----- Status -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Status_Stamina_Empty);
+
+// ----- Game Phase -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Game_Phase_Wait);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Game_Phase_Match);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Game_Phase_Loading);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Game_Phase_Play);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Game_Phase_Finish);
+
+// ----- Player State -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Ready);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Character_First);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Character_Second);
