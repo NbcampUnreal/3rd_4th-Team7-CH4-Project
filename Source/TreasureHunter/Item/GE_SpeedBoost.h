23f93d3fd3f17b5f9492bed462b0c3697dae058c
oSiris1o7 <siuesen@gmail.com>
2025-09-11 13:49:01 +0900

    [feat] 날개신발 어빌리티, 이펙트 추가

---
+++ Source/TreasureHunter/Item/GE_SpeedBoost.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "AttributeSet/THAttributeSet.h"
+#include "GE_SpeedBoost.generated.h"
+
+
+UCLASS()
+class TREASUREHUNTER_API UGE_SpeedBoost : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+
+	UGE_SpeedBoost()
+	{
+		DurationPolicy = EGameplayEffectDurationType::HasDuration;
+		DurationMagnitude=FScalableFloat(10.0f); // 10초 지속
+
+		FGameplayModifierInfo WalkModifier;
+		WalkModifier.Attribute = UTHAttributeSet::GetWalkSpeedAttribute();
+		WalkModifier.ModifierOp = EGameplayModOp::Additive;
+		WalkModifier.ModifierMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(200.0f));
+		Modifiers.Add(WalkModifier);
+
+		FGameplayModifierInfo SprintModifier;
+		SprintModifier.Attribute = UTHAttributeSet::GetSprintSpeedAttribute();
+		SprintModifier.ModifierOp = EGameplayModOp::Additive;
+		SprintModifier.ModifierMagnitude = FGameplayEffectModifierMagnitude(FScalableFloat(500.0f));
+		Modifiers.Add(SprintModifier);
+	}
+	
+};
