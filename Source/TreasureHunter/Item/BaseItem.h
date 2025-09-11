89a12a6757ad86f282246ae8ac8374a5a088cd0f
oSiris1o7 <siuesen@gmail.com>
2025-09-11 13:46:17 +0900

    [feat] 아이템 베이스, 날개신발 추가

---
+++ Source/TreasureHunter/Item/BaseItem.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "InteractPromptWidget.h"
+#include "BaseItem.generated.h"
+
+class UStaticMeshComponent;
+class ATHPlayerCharacter;
+
+UCLASS()
+class TREASUREHUNTER_API ABaseItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ABaseItem();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Data")
+	FString ItemID;
+
+	void SetItemID(FString NewItemID);
+
+
+	virtual void Tick(float DeltaTime) override;
+
+	//아이템 매쉬가 있고 상호작용하여 획득(인벤토리에 이전 이 후 삭제)
+
+
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* OverlapSphere;
+
+	UFUNCTION()
+	void OnOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UInteractPromptWidget> InteractPromptClass;
+
+	UPROPERTY()
+	UInteractPromptWidget* InteractPromptWidget;
+
+	UFUNCTION()
+	void OnOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+
+
+	void ItemPickup(ATHPlayerCharacter* PlayerCharacter);
+
+	bool bIsPickedUp;
+	FTimerHandle PickedUpTimerHandle;
+	void PickedUpTime();
+};
