9ab46243049bd28faff51e8e2cee477420d072b2
Juyee Myeong <juyee.ming@gmail.com>
2025-09-11 15:47:03 +0900

    [feature] Inventory 구현된 거 기반으로 컨트롤러에서 HUD 인벤토리 업데이트 로직 구현
    - BindInventoryDelegates(APawn*) 헬퍼 추가: UItemInventory::OnInventorySlotChanged 구독해서 HUD에 아이템 추가로직
    - ResolveItemIcon()에서 Id기반으로 아이콘 찾음
    - HandleItemCooldownClient는 인벤토리에서 UseItem 성공적으로 처리되면 Client RPC로 불러서 HUD 업데이트 진행

--- Source/TreasureHunter/Player/THPlayerController.cpp
+++ Source/TreasureHunter/Player/THPlayerController.cpp
@@ -4,11 +4,17 @@
 #include "THPlayerController.h"
 #include "UI/THPlayerHUDWidget.h"
 #include "THPlayerState.h"
+#include "Item/ItemInventory.h"
+#include "Item/ItemDataManager.h"
+#include "Item/ItemData.h"
 
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemInterface.h"
 #include "AttributeSet/THAttributeSet.h"
+#include "Kismet/GameplayStatics.h"
 
+
+#pragma region General
 void ATHPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -46,6 +52,8 @@ void ATHPlayerController::OnPossess(APawn* aPawn)
 			PS->GiveStartupAbilities();
 		}
 	}
+
+	BindInventoryDelegates(aPawn);
 }
 
 void ATHPlayerController::OnRep_PlayerState()
@@ -63,7 +71,12 @@ void ATHPlayerController::OnRep_PlayerState()
 	{
 		PS->InitializeAbilityActorInfo(GetPawn());
 	}
+
+	BindInventoryDelegates(GetPawn());
 }
+#pragma endregion
+
+#pragma region HUD
 
 void ATHPlayerController::CreatePlayerHUD()
 {
@@ -106,3 +119,56 @@ void ATHPlayerController::InitHUDBindingsFromPlayerState()
 		}
 	}
 }
+#pragma endregion
+
+#pragma region Inventory
+
+void ATHPlayerController::BindInventoryDelegates(APawn* Pawn)
+{
+	if (!Pawn) return;
+	if (UItemInventory* Inv = Pawn->FindComponentByClass<UItemInventory>())
+	{
+		// 여기서 delegate 참고 구현후에 주석 없애주세요  
+		//Inv->OnInventorySlotChanged.AddUObject(this, &ThisClass::HandleInventorySlotChanged);
+	}
+} 
+
+void ATHPlayerController::HandleInventorySlotChanged(int32 SlotIndex, const FString& ItemID)
+{
+	if (!PlayerHUD) return;
+	if (ItemID.IsEmpty())
+	{
+		return;
+	}
+
+	if (UTexture2D* Icon = ResolveItemIcon(ItemID))
+	{
+		PlayerHUD->SetInventoryIcon(SlotIndex, Icon);
+	}
+}
+
+void ATHPlayerController::HandleItemCooldownClient(int32 SlotIndex, float Cooltime)
+{
+	if (PlayerHUD)
+	{
+		// Client RPC 같은 거 하나 만드셔서 컨트롤러에서 HUD 업데이트 될 수 있게 함수 선언해주세요. 그래서 UseItem 성공하면 불려질 수 있도록 해주시면 끝.
+		PlayerHUD->ClearInventoryIcon(SlotIndex, Cooltime);
+	}
+}
+
+UTexture2D* ATHPlayerController::ResolveItemIcon(const FString& ItemID) const
+{
+	if (AItemDataManager* DM = Cast<AItemDataManager>(UGameplayStatics::GetActorOfClass(GetWorld(), AItemDataManager::StaticClass())))
+	{
+		/* 아이템 유효성 검사 (IsValid()) 를 해야해서, 참조말고 포인터로 반환 타입 바꿔주세요!! FindItemDataByItemID 함수 
+		const FItemData* Data = DM->FindItemDataByItemID(ItemID);
+		if (Data && Data->ItemIcon.IsValid())
+		{
+			return Data->ItemIcon.LoadSynchronous();
+		}*/
+
+	}
+	return nullptr;
+}
+
+#pragma endregion
