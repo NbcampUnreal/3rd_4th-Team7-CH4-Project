703e44c2ee082b5d737f495e1e93687d3450040f
oSiris1o7 <siuesen@gmail.com>
2025-09-11 13:50:29 +0900

    [feat] 아이템 상호작용 추가

---
+++ Source/TreasureHunter/Item/InteractPromptWidget.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InteractPromptWidget.generated.h"
+
+
+UCLASS()
+class TREASUREHUNTER_API UInteractPromptWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	//생성자로 텍스트 메타 만들고
+	//텍스트 바꾸는 함수 ㄱㄱ
+	
+};
