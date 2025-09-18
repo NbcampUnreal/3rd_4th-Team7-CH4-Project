4b9647533e23d942842179f90ae025cd8ca7bd91
USSA <ussa9905@gmail.com>
2025-09-18 12:50:32 +0900

    [feat] stun 이펙트 추가
    오류 수정 후 다시 추가

---
+++ Source/TreasureHunter/GameplayEffect/PlayerStunEffect.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "PlayerStunEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASUREHUNTER_API UPlayerStunEffect : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerStunEffect(const FObjectInitializer& ObjectInitializer);
+	
+};
