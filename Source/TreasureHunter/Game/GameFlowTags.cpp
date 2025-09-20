de8ff1aa30e782e9a9686a3c94a869ef24e2a54d
Juyee Myeong <juyee.ming@gmail.com>
2025-09-20 20:13:32 +0900

    [아이템/인벤토리] 태그 추가 / 정리
    - 데이터 테이블 값과 태그 연결해서 이펙트 부여하기  위한 태그 추가하고 기존에 Item관련 태그를 GameFlowTags에 추가

--- Source/TreasureHunter/Game/GameFlowTags.cpp
+++ Source/TreasureHunter/Game/GameFlowTags.cpp
@@ -36,3 +36,17 @@ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Game_Phase_Finish, "Game.Phase.Finish", "Game
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Ready, "Player.Ready", "PlayerReady");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_First, "Player.Character.First", "BunnySelected");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_Second, "Player.Character.Second", "MouseSelected");
+
+// ----- Item Set By Caller -----
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Data_WalkDelta, "Data.WalkDelta", "SetByCaller: Walk speed delta");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Data_SprintDelta, "Data.SprintDelta", "SetByCaller: Sprint speed delta");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Data_JumpDelta, "Data.JumpDelta", "SetByCaller: Jump power delta");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Data_StaminaDelta, "Data.StaminaDelta", "SetByCaller: Stamina delta (instant)");
+
+// ----- Item Activation Tag -----
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_SpeedBoost_Active, "Item.SpeedBoost.Active", "Speed boost active");
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_JumpBoost_Active,  "Item.JumpBoost.Active",  "Jump boost active");
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_ImmunePotion_Active,"Item.ImmunePotion.Active","Immune active");
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_SpeedSlow_Active,  "Item.SpeedSlow.Active",  "Speed slow active");
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_Stun_Active,       "Item.Stun.Active",       "Stun active");
+ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_Ink_Active,        "Item.Ink.Active",        "Ink active");
