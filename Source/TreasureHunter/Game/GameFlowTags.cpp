793b1528488110371c739f570e30ec154f0c434f
Juyee Myeong <juyee.ming@gmail.com>
2025-09-22 16:41:36 +0900

    [feature] sprint 이펙트 Post process 머티리얼 추가 및 관련 게임플레이 태그 추가 및 태그 정리
    - 게임플레이 태그관련 파일들에서 태그 전체 정리 및 GameplayCue Sprinting 추가
    - Sprint 이펙트를 위해 Post Process 머티리얼 추가

--- Source/TreasureHunter/Game/GameFlowTags.cpp
+++ Source/TreasureHunter/Game/GameFlowTags.cpp
@@ -7,20 +7,21 @@ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Ability_Sprint, "Ability.Sprint", "Sprint");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Ability_Mantle, "Ability.Mantle", "Mantle");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Ability_Push, "Ability.Push", "Push");
 
-// ----- CoolDown -----
+// ----- Cooldown -----
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Cooldown_Ability_Push, "Cooldown.Ability.Push", "Push Cooldown");
 
 // ----- Event -----
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Event_Hit_Falling, "Event.Hit.Falling", "Character Hit when Falling ");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Event_Hit_Falling, "Event.Hit.Falling", "Character Hit when Falling");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Event_Movement_Stopped, "Event.Movement.Stopped", "Character Stops");
 
 // ----- Effect -----
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Effect_Stamina_Regen, "Effect.Stamina.Regen", "Stamina Regen");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Effect_Stamina_Drain, "Effect.Stamina.Drain", "Stamina drain while sprint");
 
-// ----- Movement State -----
+// ----- Movement / State -----
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_State_Movement_Sprinting, "State.Movement.Sprinting", "Sprinting state");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Status_State_Mantling, "Status.State.Mantling", "Mantling state");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_State_Debuff_Stun, "State.Debuff.Stun", "Stun Debuff state");
-
 
 // ----- Status -----
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Status_Stamina_Empty, "Status.Stamina.Empty", "No stamina");
@@ -33,9 +34,17 @@ UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Game_Phase_Play, "Game.Phase.Play", "GamePlay
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Game_Phase_Finish, "Game.Phase.Finish", "GameFinish");
 
 // ----- Player State -----
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Ready, "Player.Ready", "PlayerReady");
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_First, "Player.Character.First", "BunnySelected");
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_Second, "Player.Character.Second", "MouseSelected");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Ready, "Player.Ready", "Player Ready");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_First, "Player.Character.First", "Bunny Selected");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Player_Character_Second, "Player.Character.Second", "Mouse Selected");
 
-// ----- Event -----
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Event_Movement_Stopped, "Event.Movement.Stopped", "Character Stops");
+// ----- GameplayCue -----
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_GameplayCue_Movement_Sprinting, "GameplayCue.Movement.Sprinting", "Speedlines while sprinting");
+
+// ----- Item Effects -----
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_SpeedBoost_Active, "Item.SpeedBoost.Active", "Speed boost effect is active.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_JumpBoost_Active, "Item.JumpBoost.Active", "Jump boost effect is active.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_ImmunePotion_Active, "Item.ImmunePotion.Active", "Immune potion effect is active.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_SpeedSlow_Active, "Item.SpeedSlow.Active", "Speed Slow effect is active.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_Stun_Active, "Item.Stun.Active", "Stun effect is active.");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Item_Ink_Active, "Item.Ink.Active", "Ink effect is active.");
--- Source/TreasureHunter/Game/GameFlowTags.h
+++ Source/TreasureHunter/Game/GameFlowTags.h
@@ -8,16 +8,18 @@ UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Sprint);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Mantle);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_Push);
 
-// ----- CoolDown -----
+// ----- Cooldown -----
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Cooldown_Ability_Push);
 
 // ----- Event -----
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Event_Hit_Falling);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Event_Movement_Stopped);
 
 // ----- Effect -----
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Effect_Stamina_Regen);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Effect_Stamina_Drain);
 
-// ----- Movement State -----
+// ----- Movement / State -----
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_State_Movement_Sprinting);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Status_State_Mantling);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_State_Debuff_Stun);
@@ -37,5 +39,13 @@ UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Ready);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Character_First);
 UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Player_Character_Second);
 
-// ----- Event -----
-UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Event_Movement_Stopped);
+// ----- GameplayCue -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_GameplayCue_Movement_Sprinting);
+
+// ----- Item Effects -----
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_SpeedBoost_Active);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_JumpBoost_Active);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_ImmunePotion_Active);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_SpeedSlow_Active);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_Stun_Active);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Item_Ink_Active);
